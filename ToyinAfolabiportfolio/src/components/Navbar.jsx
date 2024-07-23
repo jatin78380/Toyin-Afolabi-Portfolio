036eb3c9ff64a498361e9d422d52c5261bfadf9a
jatinssh <jatin7838066@gmail.com>
2024-07-23 12:24:09 +0530

    updated

--- ToyinAfolabiportfolio/src/components/Navbar.jsx
+++ ToyinAfolabiportfolio/src/components/Navbar.jsx
@@ -1,27 +1,20 @@
-// import JPP from "../assets/JPP.png";
-import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { SiLeetcode } from "react-icons/si";
-
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
 export const Navbar = () => {
   return (
-    <div> 
-        <nav className=" mb-20 flex items-center justify-between py-6">
+    <div>
+      <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-          <img className="mx-2 w-10 rounded"  alt="logo"/> 
+          <img className="mx-2 w-10 rounded" alt="logo" />
         </div>
-        <div className="m-8 flex itens-center justify-center gap-4 text-2xl">
-         <a href="https://www.linkedin.com/in/toyin-afolabi-/" className="text-blue-600" target="_blank">
-          <FaLinkedin />
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+          <a href="https://www.linkedin.com/in/toyin-afolabi-/" className="text-blue-600" target="_blank">
+            <FaLinkedin />
           </a>
-          <a href="https://www.facebook.com/toyin.afolabi.92" className="text-blue-500" target="_blank">
-          <FaFacebook/>
+          <a href="https://www.instagram.com/dhebbysmart?igsh=dm16aGU4enM1Yjlz" className="text-pink-500" target="_blank">
+            <FaInstagram />
           </a>
-
         </div>
-        </nav>
-
-
-      </div>
-  )
-}
+      </nav>
+    </div>
+  );
+};
