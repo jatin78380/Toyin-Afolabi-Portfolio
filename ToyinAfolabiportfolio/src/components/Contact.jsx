699e721b4f28faf9f4857ec5be9e065144c1beec
jatinssh <jatin7838066@gmail.com>
2024-08-03 15:31:29 +0530

    updated contact code

--- ToyinAfolabiportfolio/src/components/Contact.jsx
+++ ToyinAfolabiportfolio/src/components/Contact.jsx
@@ -1,37 +1,53 @@
 import React from 'react';
-import { FaEnvelope, FaPhone } from 'react-icons/fa';
+import { FaEnvelope, FaPhone, FaLinkedin } from 'react-icons/fa';
+import { motion } from "framer-motion";
 
-const ContactCard = ({ icon, title, detail, additionalDetail,email }) => {
+const ContactCard = ({ icon, title, detail, additionalDetail, link }) => {
   return (
     <div className="bg-orange-500 rounded-lg p-6 mb-4 flex flex-col items-center text-white">
       <div className="mb-2">{icon}</div>
       <h3 className="text-lg font-bold">{title}</h3>
       <p className="text-base">{detail}</p>
-      {additionalDetail && (
-        <a href={`mailto:${email}`} className="text-sm text-gray-200">
+      {additionalDetail && link ? (
+        <a href={link} className="text-sm text-gray-200" target="_blank" rel="noopener noreferrer">
           {additionalDetail}
         </a>
-      )}
+      ) : null}
     </div>
   );
 };
 
 const Contact = () => {
+  const email = "afolabitoyin253@gmail.com";
+  const linkedinUrl = "https://www.linkedin.com/in/toyin-afolabi-/"; // Replace with your LinkedIn profile URL
+
   return (
-    <div className="min-h-screen  text-black flex flex-col items-center pt-10">
-      <h2 className="text-sm mb-2 font-bold">Get In Touch</h2>
+    <div className="min-h-screen text-black flex flex-col items-center pt-10">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-neutral-800 text-4xl"
+      >Get In Touch
+      </motion.h1>
       <h1 className="text-3xl font-bold mb-10 text-neutral-800">Contact Me</h1>
       <ContactCard
         icon={<FaEnvelope size={30} />}
         title="Email"
-        detail="afolabitoyin253@gmail.com"
-        additionalDetail="Send Email"
-        email = "afolabitoyin253@gmail.com"
+        detail={email}
+        additionalDetail="Send a message on email"
+        link={`mailto:${email}`}
       />
       <ContactCard
         icon={<FaPhone size={30} />}
         title="Phone"
         detail="+2349027883008"
+      />
+      <ContactCard
+        icon={<FaLinkedin size={30} />}
+        title="LinkedIn"
+        additionalDetail="View Profile"
+        link={linkedinUrl}
       />
     </div>
   );
